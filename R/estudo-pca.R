a732481c5c65597cfcf1c0ea16df3fc6f7a7b58b
Leonardo Leite <leonardofl87@gmail.com>
2016-05-24 22:09:06 -0300

    mais anotações

--- R/estudo-pca.R
+++ R/estudo-pca.R
@@ -4,6 +4,8 @@ csv_file <- 'dados/votes-cmsp-2015.csv'
 data  <- read.csv(csv_file, sep=',', as.is=T)
 votes_matrix <- with(data, tapply(vote, list(voter_id, rollcall), c))
 votes_matrix[is.na(votes_matrix)] <- 0 # Transforma NAs em 0s
+dim(votes_matrix)
+# votes_matrix: 67 vereadores X 131 votações
 
 # Esse
 s <- svd( votes_matrix - mean(votes_matrix) )
@@ -12,6 +14,8 @@ plot(s$d[1]*s$v[,1], s$d[2]*s$v[,2])
 pca <- prcomp( t(votes_matrix) )
 plot(pca$x[,1], pca$x[,2])
 # Mas temos 131 pontos, o que corresponde às votações, e não aos vereadores.
+
+# prcomp assumes: units/samples in row and features in columns.
 
 # Esse
 s <- svd( t(votes_matrix) - mean(t(votes_matrix)) )
