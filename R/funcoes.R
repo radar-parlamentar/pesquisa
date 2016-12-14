55747cfa5a5b2bf5ce6f628e64f8a4a37875e169
Leonardo Leite <leonardofl87@gmail.com>
2016-12-13 22:14:03 -0200

    exponencial -> gaussiana

--- R/funcoes.R
+++ R/funcoes.R
@@ -11,7 +11,7 @@ mypar(1,2)
 x <- seq(-4, 4, length=100)
 p <- dnorm(x)*10
 plot(x, p, type="l", xlab="x", ylab="U",
-     main="Função exponencial")
+     main="Função gaussiana")
 x <- seq(-4, 4, length=100)
 p <- (-x^2 + 15)/3.5
 plot(x, p, type="l", xlab="x", ylab="U",
